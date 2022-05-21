f0cdc6e3d3aa0bef082a30e9ad433757a27c6198
Gitkolan <maqbool62@gmail.com>
2022-05-21 16:05:10 +0300

    My PA2
    My Programming Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
+## create a vector and assign value numbers and get values and get mean, reset different
+##value in the Vector and calculate cachemean of the vector and retrieve cached mean.
+
 ## functions do
+aVector <- makeVector(1:10)
+aVector$get() #retrieve the value of X
+aVector$getmean() #retrieve the value of m , it's value should be NULL
+aVector$set(30:50)
+cachemean(aVector) #note mean is calculated for the set new value 30:50 NOT for 1:10
+aVector$getmean() #retrieve it directly, now it has been cached
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## MakeCacheMatrix, here creating in it a 
+## Matrix of 2 rows, 2 columns for the values assigned in the matrix and get the values of function
+## get inverse which is NULL value and cache solve for getting cached data.
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
